a372ee3074aa55d9afdfabc17c8fb903c24065dd
Furkan Uğuz <oguzfurkanss@gmail.com>
2022-12-11 22:53:12 +0300

    feat: Added basket store and add, remove, update to homepage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.scss';
 
 import HomePage from './pages/Home';
+import BasketStore from './store/Basket';
 
 function App() {
-  return <HomePage />;
+  return (
+    <BasketStore>
+      <HomePage />
+    </BasketStore>
+  );
 }
 
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import BultenList from '../../data/bulten_data.json';
 import VirtualEventTable from '../../components/VirtualEventTable/VirtualEventTable';
 import useBulten from '../../hooks/Bulten';
+import Basket from '../../components/Basket/Basket';
+import { BasketContext } from '../../store/Basket';
+import {
+  addEvent,
+  removeEvent,
+  updateBetType,
+} from '../../store/Basket/actions';
 
 function HomePage() {
   const { events } = useBulten(BultenList);
+  const [isBasketVisible, setIsBasketVisible] = useState(false);
 
-  return <VirtualEventTable events={events} />;
+  const { state: basketItems, dispatch } = useContext(BasketContext);
+
+  const handleSelectBet = (event) => {
+    const isAddedBet = basketItems.find((bet) => bet.id === event.id);
+
+    if (!isAddedBet) {
+      dispatch(addEvent(event));
+    } else if (event.betTypeId === isAddedBet.betTypeId) {
+      dispatch(removeEvent(event.id));
+    } else {
+      dispatch(updateBetType(event));
+    }
+  };
+
+  return (
+    <>
+      <VirtualEventTable
+        events={events}
+        selectedBets={basketItems}
+        handleSelectBet={handleSelectBet}
+      />
+      <Basket
+        visible={isBasketVisible}
+        handleToggleBasket={() => setIsBasketVisible(!isBasketVisible)}
+        events={basketItems}
+      />
+    </>
+  );
 }
 
 export default HomePage;
